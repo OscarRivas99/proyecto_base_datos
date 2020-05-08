f359167cc2e2a744a21e342b922f54d8bccc43f5
AlexyssAyala <00238918@uca.edu.sv>
2020-05-08 00:26:53 -0600

    p
    prueba2

---
+++ app/src/main/java/com/example/menu_app/ui/gallery/CategoriFragment.kt
@@ -0,0 +1,25 @@
+package com.example.menu_app.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.menu_app.R
+
+class CategoriFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_categoria, container, false)
+        val textView: TextView = root.findViewById(R.id.txt_categoria)
+        return root
+    }
+}
