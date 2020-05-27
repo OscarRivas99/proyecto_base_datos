3e2c3e8661cd0d90b21ef149ad81b55c8e586289
AlexyssAyala <00238918@uca.edu.sv>
2020-05-26 22:27:43 -0600

    fix_error_2

--- app/src/main/java/com/example/menu_app/MainActivity.kt
+++ app/src/main/java/com/example/menu_app/MainActivity.kt
@@ -32,6 +32,9 @@ class MainActivity : AppCompatActivity() {
 
 
         
+
+
+        
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
