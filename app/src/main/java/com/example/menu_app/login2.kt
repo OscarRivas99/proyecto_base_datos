bf70bd28c377c6b2d6bed4a79c7e09ecdb2f8265
AlexyssAyala <00238918@uca.edu.sv>
2020-05-27 18:23:02 -0600

    login_linked

--- app/src/main/java/com/example/menu_app/login2.kt
+++ app/src/main/java/com/example/menu_app/login2.kt
@@ -3,6 +3,8 @@ package com.example.menu_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login2.*
 
 class login2 : AppCompatActivity() {
@@ -10,9 +12,17 @@ class login2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
        btn1.setOnClickListener {
-           val intent = Intent(this,MainActivity::class.java)
-           startActivity(intent)
+           val user_name = usuario.text.trim().toString();
+           val password = pass.text.trim().toString();
+           if ((user_name.equals("root",true) && (password.equals("root", true))) ){
+               val intent = Intent(this,MainActivity::class.java)
+               startActivity(intent)
+           }else{
+               Toast.makeText(this, "usuario o contraseña incorrectas"+ usuario.text.toString() ,+ Toast.LENGTH_LONG).show()
+           }
+
        }
     }
 }
