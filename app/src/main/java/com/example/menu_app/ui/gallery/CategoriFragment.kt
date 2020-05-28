db86a6adeb5f3a893d2a4c9a8a6b5aa506a6052c
KennetQ <00114718@uca.edu.sv>
2020-05-28 14:03:17 -0600

    Vista categoria terminada

--- app/src/main/java/com/example/menu_app/ui/gallery/CategoriFragment.kt
+++ app/src/main/java/com/example/menu_app/ui/gallery/CategoriFragment.kt
@@ -4,25 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.databinding.DataBindingUtil
+
+
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+
 import com.example.menu_app.R
-import com.example.menu_app.databinding.FragmentCategoriaBinding
-import com.example.menu_app.databinding.FragmentHomeBinding
+
+
 
 class CategoriFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
-
-        val binding = DataBindingUtil.inflate<FragmentCategoriaBinding>(inflater, R.layout.fragment_categoria, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_categoria, container, false)
     }
 }
+
