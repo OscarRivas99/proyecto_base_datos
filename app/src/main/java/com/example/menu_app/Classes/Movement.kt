d0e7100a262f9736b8a89060d53592bdac3619a1
OscarRivas99 <54113699+OscarRivas99@users.noreply.github.com>
2020-06-09 18:33:04 -0600

    proyecto papa

---
+++ app/src/main/java/com/example/menu_app/Classes/Movement.kt
@@ -0,0 +1,12 @@
+package com.example.menu_app.Classes
+
+class Movement {
+
+    var id: Long = -1
+    var name = ""
+    var username = ""
+    var date = " "
+    var createdAt = ""
+
+
+}
--- app/src/main/java/com/example/menu_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/menu_app/ui/home/HomeFragment.kt
@@ -7,33 +7,25 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.menu_app.R
-import com.example.menu_app.databinding.FragmentHomeBinding
-import com.example.menu_app.databinding.FragmentMetodosBinding
+
 
 
 class HomeFragment : Fragment() {
 
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
-    val button = binding.categoriesButton
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_home_to_nav_categoria)
-        }
-
-
-
-
-        return binding.root
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
+
